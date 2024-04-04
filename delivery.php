7a5caa655c938ceed27e82c9335fe09d7f17e953
BrytonNgaks <146178625+BrytonNgaks@users.noreply.github.com>
2024-04-04 18:13:36 +0300

    Update delivery.php

--- delivery.php
+++ delivery.php
@@ -1,1 +1,30 @@
 
+<?php  ?><!DOCTYPE html>
+<html>
+<head>
+       <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Delivery Page</title>
+</head>
+<body>
+    <h1>Delivering to your specific location in less than 25minutes. </h1>
+    <h2>THANK YOU FOR ORDERING WITH FOODHUB</h2>
+    <div id="delivery-location"></div>
+<style>
+    body{
+        background-color: orange;
+    }
+</style>
+    <script>
+        // Function to extract location from URL query parameter
+        function getLocationFromURL() {
+            var urlParams = new URLSearchParams(window.location.search);
+            return urlParams.get('location');
+        }
+
+        // Update the delivery location content
+        var deliveryLocation = getLocationFromURL();
+        document.getElementById("delivery-location").innerText = deliveryLocation;
+    </script>
+</body>
+</html>
+
